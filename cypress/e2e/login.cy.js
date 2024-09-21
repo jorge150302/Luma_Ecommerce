17b8e69f503da8178a2154b95378a634b9e90633
Jorge Almanzar <jorgeluisalmanzar@gmail.com>
2024-09-21 18:31:13 -0400

    LoginUpdates

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,18 @@
 import notification from "./components/notification"
 
+
 describe('Login tests', () => {
 
     beforeEach(() =>{
       cy.visit('/customer/account/login/')
       cy.fixture('users').as('user');
       cy.fixture('locators').as('element');
+      cy.fixture('LoginData').as('LoginData');
     })
 
     it('login successfuly', function() {
         cy.login(this.user.email, this.user.password)
-        notification.checkAccount('My Dashboard')
+        notification.checkAccount(this.LoginData.MyDashboard)
 
     })
 
@@ -18,14 +20,14 @@ describe('Login tests', () => {
         cy.get(this.element.passwordinput).type(this.user.password);
         cy.get(this.element.submitbutton).click();
         //cy.get(this.element.requiredemail).should('have.text', 'This is a required field.')
-        notification.checkEmail('This is a required field.');
+        notification.checkEmail(this.LoginData.RequiredField);
       })
 
     it('login - password empty', function() {
       cy.get(this.element.emailinput).type(this.user.email);
       cy.get(this.element.submitbutton).click();
       //cy.get(this.element.requiredpass).should('have.text', 'This is a required field.')
-      notification.checkPassword('This is a required field.');
+      notification.checkPassword(this.LoginData.RequiredField);
       })
        
       it('login - wrong password', function() {
@@ -33,7 +35,7 @@ describe('Login tests', () => {
         cy.get(this.element.passwordinput).type('welcome2');
         cy.get(this.element.submitbutton).click();
         //cy.get(this.element.invalidLoginOrPassword).should('have.text', 'Invalid login or password.');
-        notification.checkLogin('Invalid login or password.');
+        notification.checkLogin(this.LoginData.InvalidMessage);
       })
   
       it('login - wrong email', function() {
@@ -41,7 +43,7 @@ describe('Login tests', () => {
         cy.get(this.element.passwordinput).type(this.user.password);
         cy.get(this.element.submitbutton).click();
         //cy.get(this.element.invalidLoginOrPassword).should('have.text', 'Invalid login or password.');
-        notification.checkLogin('Invalid login or password.');
+        notification.checkLogin(this.LoginData.InvalidMessage);
       })
 
       it('forgot your password?', function() {
@@ -49,7 +51,7 @@ describe('Login tests', () => {
         cy.get(this.element.forgotPassworEmailInput).type('pruebatesting654@gmail.com');
         cy.get(this.element.forgotPassworSubmitButton).click();
         //cy.get(this.element.successMessageForgotEmail).should('contain', 'you will receive an email with a link to reset your password');
-        notification.checkSuccessMessage('you will receive an email with a link to reset your password');
+        notification.checkSuccessMessage(this.LoginData.ForgetMessage);
        })
        
 
